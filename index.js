bf32f755af092e84921b2a64073abd3fa72c3dfa
Kamal Patel <kamalpatel@Kamals-MacBook-Pro.local>
2022-01-18 17:01:01 +0530

    bug fixing

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-// import App from './App';
-// import App from './App2';
 
-// import App from './App3'; // STACK NAVIGATION screen navigation
-import App from './App5'; // Bottom Tabs Navigator
-// import App from './App4'; // Drawer Navigator
+import App from './src/App';
 
 import {name as appName} from './app.json';
 
